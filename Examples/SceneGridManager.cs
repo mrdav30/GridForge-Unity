403bd932df230de56193116ea6a39f10f979494a
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2025-02-19 02:19:32 -0500

    task: add examples to use in Unity

---
+++ Examples/SceneGridManager.cs
@@ -0,0 +1,21 @@
+using GridForge.Configuration.Unity_Editor;
+using GridForge.Grids;
+using UnityEngine;
+
+public class SceneGridManager : MonoBehaviour
+{
+    GridConfigurationSaver configurationSaver;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        configurationSaver = gameObject.GetComponent<GridConfigurationSaver>();
+        GlobalGridManager.Setup(configurationSaver.NodeSize, configurationSaver.SpatialGridCellSize);
+        gameObject.GetComponent<GridConfigurationSaver>().EarlyApply();
+    }
+
+    private void OnApplicationQuit()
+    {
+        GlobalGridManager.Reset();
+    }
+}
---
+++ Runtime/Blockers/BlockerComponent.cs
@@ -0,0 +1,84 @@
+﻿#if UNITY_EDITOR
+using FixedMathSharp;
+using GridForge.Grids;
+using UnityEngine;
+
+namespace GridForge.Blockers.Unity_Editor
+{
+    /// <summary>
+    /// Available blocker types for Unity selection.
+    /// </summary>
+    public enum BlockerType
+    {
+        None = 0,
+        Bounds = 1
+    }
+
+    /// <summary>
+    /// Unity component that allows selection of a blocker type in the Inspector.
+    /// </summary>
+    [DisallowMultipleComponent]
+    public class BlockerComponent : MonoBehaviour
+    {
+#pragma warning disable CS0649 // These fields are serialized via Unity Editor
+        [SerializeField] private BlockerType _blockerType;
+
+        [SerializeField] private bool _isActive;
+
+        [SerializeField] private bool _cacheCoveredNodes;
+
+        [SerializeField] private BoundingArea _manualBlockArea;
+#pragma warning restore CS0649
+
+        private IBlocker _blocker;
+
+        public bool IsSet;
+
+        public void Awake()
+        {
+            _blocker = BlockerFactory.CreateBlocker(_blockerType, this);
+        }
+
+        public void Start()
+        {
+            if (!GlobalGridManager.IsActive)
+                return;
+            _blocker.ApplyBlockage();
+            IsSet = true;
+        }
+
+        public void OnEnable()
+        {
+            if (!GlobalGridManager.IsActive || IsSet)
+                return;
+            _blocker.ApplyBlockage();
+            IsSet = true;
+        }
+
+        public void OnDisable()
+        {
+            _blocker?.RemoveBlockage();
+            IsSet = false;
+        }
+
+        public bool IsActive => _isActive;
+        public bool CacheCoveredNodes => _cacheCoveredNodes;
+        public BoundingArea ManualBlockArea => _manualBlockArea;
+    }
+
+    public static class BlockerFactory
+    {
+        public static IBlocker CreateBlocker(BlockerType type, BlockerComponent component)
+        {
+            return type switch
+            {
+                BlockerType.Bounds => new BoundsBlocker(
+                    component.ManualBlockArea,
+                    component.IsActive,
+                    component.CacheCoveredNodes),
+                _ => null
+            };
+        }
+    }
+}
+#endif
